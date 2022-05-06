512ced4a9c5e8ae7fdc497ed07fb2941244aa48c
aylaamaral2106 <102435778+aylaamaral2106@users.noreply.github.com>
2022-05-06 15:59:33 -0300

    Add files via upload

---
+++ atividade5.py
@@ -0,0 +1,9 @@
+comprimento = input("Digite o comprimento: ")
+largura = input("Digite a largura: ")
+ 
+calculo = float(comprimento) * float(largura)
+produto = float(calculo/2)
+
+resultadoformatado = "a Area a ser limpa é de " + str(calculo) +" metros quadrados, " "e o produto a ser ultilizado é de " + str(produto) + " litros."
+
+print(resultadoformatado)
